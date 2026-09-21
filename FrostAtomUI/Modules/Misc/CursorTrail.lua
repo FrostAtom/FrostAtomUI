42f760ba7e9ca4e3f3f7646b197359d3edb53446
atom <vgolumbivskii@ukr.net>
2026-09-21 16:08:51 +0400

    CursorTrail: tone down bolt and shine (smaller scale, lower alpha)

--- FrostAtomUI/Modules/Misc/CursorTrail.lua
+++ FrostAtomUI/Modules/Misc/CursorTrail.lua
@@ -9,12 +9,14 @@ local trail = CreateFrame("Model")
 trail:SetAllPoints()
 trail:SetFrameStrata("FULLSCREEN_DIALOG")
 trail:SetModel("spells\\lightningboltivus_missile.mdx")
-trail:SetModelScale(0.0024)
+trail:SetModelScale(0.0018)
+trail:SetAlpha(0.6)
 
 local shine = CreateFrame("Model", nil, trail)
 shine:SetAllPoints()
 shine:SetModel("spells\\manafunnel_impact_chest.mdx")
-shine:SetModelScale(0.014)
+shine:SetModelScale(0.01)
+shine:SetAlpha(0.5)
 
 local screenDiagonal = (GetScreenWidth() ^ 2 + GetScreenHeight() ^ 2) ^ 0.5
 local lastX, lastY
